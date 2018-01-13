72e50f55f5869c99d568567f5ba53abd97b540db
Mardav Wala <tru.vinci@gmail.com>
2018-01-13 18:26:39 -0500

    Rename file

--- src/components/Filter.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="search">
-        <label for="zip">Enter zip code</label>
-        <input type="search" name="zip" id="zip" v-model="search">
-    </div>
-</template>
---
+++ src/components/MapFilter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="search">
+        <label for="zip">Enter zip code</label>
+        <input
+            type="search"
+            name="zip"
+            id="zip"
+            :value="zip"
+            @keyup.enter="updateZip($event.target.value)"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MapFilter",
+
+    props: {
+        zip: {
+            type: Number,
+            default: 0
+        },
+        updateZip: {
+            type: Function,
+            required: true
+        }
+    }
+};
+</script>
+
